347a13e1e67641bc5cf4211a5b6408eb2811bcaa
unknownkirya <unknownkirya@gmail.com>
2024-05-12 15:08:02 +0300

    Initial Commit

---
+++ SmartTextRecognizer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartTextRecognizer
+//
+//  Created by Кирилл Бережной on 12.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
